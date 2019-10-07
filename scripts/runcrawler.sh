a2d4c9771cb1cf4aef7da97c707528262cbd714a
Divyanshu Kakwani <divkakwani@gmail.com>
2019-10-08 01:19:35 +0530

    Add stats script

--- scripts/runcrawler.sh
+++ scripts/runcrawler.sh
@@ -12,11 +12,11 @@ do
     SECONDS=0
 
     # run with a timeout of 6 hours
-    python3 main.py fetch-news --lang kn --srange 8,20 --timeout 21600
+    python3 main.py fetch-news --lang kn --srange 28,29 --timeout 21600
     wait $!
 
     duration=$SECONDS
-    if (( duration > 60 ));
+    if (( duration > 120 ));
     then
         echo "Creating Checkpoint..."
         cp -r data/job "data/chkpt-$chkpt"
