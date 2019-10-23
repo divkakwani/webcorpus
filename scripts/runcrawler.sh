ad05b2336516b2d8194f915277fe02b5168262c3
Gokul NC <gokulnc@ymail.com>
2019-10-23 16:51:50 +0000

    Save logs locally (in-case we need it)

--- scripts/runcrawler.sh
+++ scripts/runcrawler.sh
@@ -8,13 +8,14 @@ then
 fi
 
 chkpt=0
+mkdir -p logs
 
 while true
 do
     SECONDS=0
 
     # run with a timeout of 6 hours
-    corpora fetch-news --lang kn --srange 28,29 --timeout 21600
+    corpora fetch-news --lang kn --srange 28,29 --timeout 21600 2>&1 | tee logs/kn_logs.txt
     wait $!
 
     duration=$SECONDS
