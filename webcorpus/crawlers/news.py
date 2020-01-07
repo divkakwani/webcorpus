0f8cd9655687e663f674783e7dc7fd65c082bdcf
Divyanshu Kakwani <divkakwani@gmail.com>
2020-01-07 15:02:46 +0530

    update news crawling classes

--- webcorpus/crawlers/news.py
+++ webcorpus/crawlers/news.py
@@ -22,9 +22,11 @@ import tldextract
 import sys
 import inspect
 import importlib
+import re
 
 from boilerpipe.extract import Extractor
 from scrapy.linkextractors import LinkExtractor
+from scrapy.linkextractors import Selector
 from ..corpus.io import CatCorpus
 from ..language import code2script, in_script
 from ..utils import validate_url
@@ -62,14 +64,11 @@ def _select_baseclass(source):
     return None
 
 
-def makecrawler(source, srcdir, **settings):
+def makecrawler(source, **settings):
     """
     creates a new spider class by cloning one of the base classes
     and then populating the custom settings appropriately
     """
-    sys.path.append(srcdir)
-    globals().update(importlib.import_module('custom_handlers').__dict__)
-
     baseclass = _select_baseclass(source)
     if baseclass is None:
         return None
@@ -89,54 +88,56 @@ def makecrawler(source, srcdir, **settings):
 class BaseNewsSpider(scrapy.Spider):
 
     custom_settings = {
-        "DOWNLOAD_DELAY": 0.05,
-        "LOG_ENABLED": True,
-        "CONCURRENT_REQUESTS": 64,
-        "AUTOTHROTTLE_ENABLED": True,
+        'DOWNLOAD_DELAY': 0.05,
+        'LOG_ENABLED': True,
+        'CONCURRENT_REQUESTS': 1024,
+        'AUTOTHROTTLE_ENABLED': True,
     }
 
-    def __init__(self, source, corpus_path, html_path=None):
+    def __init__(self, source, arts_path, html_path):
         self.lang = source['lang']
         self.script = code2script(self.lang)
         self.name = source['name']
-        self.corpus_path = corpus_path
+        self.arts_path = arts_path
+        self.html_path = html_path
         self.arts_collected = 0
 
-        os.makedirs(self.corpus_path, exist_ok=True)
+        os.makedirs(self.arts_path, exist_ok=True)
+        os.makedirs(self.html_path, exist_ok=True)
 
-        parts = tldextract.extract(source["home_url"])
-        domain = "{}.{}.{}".format(parts.subdomain, parts.domain, parts.suffix)
+        parts = tldextract.extract(source['home_url'])
+        domain = '{}.{}.{}'.format(parts.subdomain, parts.domain, parts.suffix)
         self.allowed_domains = [domain]
 
-        self.corpus = CatCorpus(self.corpus_path)
-        if html_path:
-            self.html_corpus = CatCorpus(html_path)
+        self.arts_corpus = CatCorpus(self.arts_path)
+        self.html_corpus = CatCorpus(self.html_path)
 
         super().__init__(self.name)
 
     def extract_article_content(self, html):
-        extractor = Extractor(extractor="ArticleExtractor", html=html)
-        text = extractor.getText()
-        return text
+        extractor = Extractor(extractor='ArticleExtractor', html=html)
+        body = extractor.getText()
+        title = extractor.source.title
+        return {'title': title, 'body': body}
 
     def parse_article(self, response):
         """
         Extracts the article content from the response body and prepares
         the article object
         """
-        text = self.extract_article_content(response.body)
-        if self._is_article(text):
+        content = self.extract_article_content(response.body)
+        if self._is_article(content.body):
             article = {
-                'content': text,
+                'title': content['title'],
+                'body': content['body'],
                 'source': self.name,
                 'url': response.request.url,
-                'raw_html': response.body.decode(response.encoding),
                 'timestamp': datetime.now().strftime('%d/%m/%y %H:%M')
             }
             return article
         return None
 
-    def _is_article(self, text, win_sz=300, thres=260):
+    def _is_article(self, text, win_sz=250, thres=200):
         """
         It performs two tests on the text to determine if the text represents
         a valid news article or not:
@@ -162,7 +163,11 @@ class BaseNewsSpider(scrapy.Spider):
     def write_article(self, article):
         dump = json.dumps(article, indent=4, ensure_ascii=False)
         self.arts_collected += 1
-        self.corpus.add_file(article["source"], article["url"], dump)
+        self.arts_corpus.add_file(article['source'], article['url'], dump)
+
+    def write_html(self, response):
+        html = response.body.decode(response.encoding)
+        self.html_corpus.add_file(self.name, response.request.url, html)
 
     def parse(self, response):
         raise NotImplementedError
@@ -177,6 +182,7 @@ class SitemapSpider(BaseNewsSpider, scrapy.spiders.SitemapSpider):
         article = self.parse_article(response)
         if article:
             self.write_article(article)
+        self.write_html(response)
 
 
 class RecursiveSpider(BaseNewsSpider):
@@ -189,7 +195,72 @@ class RecursiveSpider(BaseNewsSpider):
         article = self.parse_article(response)
         if article:
             self.write_article(article)
+        self.write_html(response)
 
         links = self.link_extractor.extract_links(response)
         for link in links:
             yield scrapy.Request(link.url)
+
+
+def extract_links(text):
+    return re.findall(r'(https?://[^\s"\\<>]+)', text)
+
+
+class SanjevaniSpider(RecursiveSpider):
+    """
+    Boilerpipe does not work for this news source
+    """
+
+    def extract_article_content(self, html):
+        sel = Selector(text=html)
+        text = ''
+        for node in sel.css('.entry-content *::text'):
+            text = text + '\n' + node.extract()
+        return text
+
+
+class BalkaniNewsSpider(SitemapSpider):
+    """
+    The urls contained in the sitemap do not directly link to the page
+    where the complete article exists
+    """
+
+    def __init__(self, source, corpus_path):
+        super().__init__(source, corpus_path)
+        self.link_extractor = LinkExtractor()
+
+    def parse(self, response):
+        article = self.parse_article(response)
+        if article:
+            self.write_article(article)
+        self.write_html(response)
+
+        links = self.link_extractor.extract_links(response)
+        for link in links:
+            yield scrapy.Request(link.url)
+
+
+class AnupambharatonlineSpider(RecursiveSpider):
+    custom_settings = {
+        'DUPEFILTER_CLASS': 'scrapy.dupefilters.BaseDupeFilter'
+    }
+
+
+class SahilOnlineSpider(BaseNewsSpider):
+    """
+    The sitemap is broken, invalid xml. But we can still extract the urls
+    using scrapy's LinkExtractor
+    """
+
+    def __init__(self, source, corpus_path):
+        response = requests.get(source['sitemap_url'])
+        urls = extract_links(str(response.content))
+        urls = list(filter(lambda u: not u.lower().endswith('jpg'), urls))
+        self.start_urls = urls
+        super().__init__(source, corpus_path)
+
+    def parse(self, response):
+        article = self.parse_article(response)
+        if article:
+            self.write_article(article)
+        self.write_html(response)
