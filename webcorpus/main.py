cc1cd38381555b52c57dbe29be03d93032b41feb
Divyanshu Kakwani <divkakwani@gmail.com>
2020-01-21 22:50:07 +0530

    implement process subcommand in cli

--- webcorpus/main.py
+++ webcorpus/main.py
@@ -4,6 +4,8 @@ import logging
 from scrapy.crawler import CrawlerProcess
 from .corpus.stats import print_stats
 from .crawlers.w3newspaper import W3NewsPaperSpider
+from .processors.arts import ArtsProcessor
+from .processors.sent import SentProcessor
 
 
 @click.group()
@@ -23,18 +25,31 @@ def get_sources(langs, srcdir):
     process.start()
 
 
-@cli.command(name='fetch')
+# @cli.command(name='fetch')
+# @click.option('--lang', required=True)
+# @click.option('--srcdir', required=True)
+# @click.option('--jobdir_root', required=True)
+# @click.option('--output', required=True)
+# @click.option('--logfile', default=None)
+# def fetch(lang, srcdir, jobdir_root, output, logfile):
+#     if logfile:
+#         logging.basicConfig(filename=logfile)
+#     else:
+#         logging.basicConfig(level=logging.WARN)
+#     fetch_corpus(lang, output, srcdir, jobdir_root)
+
+
+@cli.command(name='process')
 @click.option('--lang', required=True)
-@click.option('--srcdir', required=True)
-@click.option('--jobdir_root', required=True)
+@click.option('--dtype', required=True)
+@click.option('--input', required=True)
 @click.option('--output', required=True)
-@click.option('--logfile', default=None)
-def fetch(lang, srcdir, jobdir_root, output, logfile):
-    if logfile:
-        logging.basicConfig(filename=logfile)
-    else:
-        logging.basicConfig(level=logging.WARN)
-    fetch_corpus(lang, output, srcdir, jobdir_root)
+def process(lang, dtype, input, output):
+    if dtype == 'arts':
+        processor = ArtsProcessor(lang, input, output)
+    elif dtype == 'sent':
+        processor = SentProcessor(lang, input, output)
+    processor.gen_dataset()
 
 
 @cli.command(name='stats')
