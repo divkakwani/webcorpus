2f0fd0edf4ea816df6c1ad6bda23b28e55bed840
divkakwani <divkakwani@gmail.com>
2020-05-05 00:15:38 +0530

    add sync sources script

---
+++ scripts/sync-sources.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import random
+import os
+import sys
+
+from webcorpus.sources import Sources
+from firebase_admin import credentials, firestore, initialize_app
+
+
+if len(sys.argv) != 2:
+    print('Usage: ./sync-sources.py <src-dir> <firestore-key-path>')
+else:
+    srcdir, key_path = sys.argv[1], sys.argv[1]
+
+
+# Initialize Firestore DB
+cred = credentials.Certificate(key_path)
+default_app = initialize_app(cred)
+db = firestore.client()
+
+files = list(filter(lambda s: s.endswith('csv'), os.listdir(srcdir)))
+langs = [os.path.splitext(f)[0] for f in files]
+
+for lang, file in zip(langs, files):
+    sources = list(Sources(lang, os.path.join(srcdir, file)))
+    random.shuffle(sources)
+    doc_ref = db.collection('sources').document(lang)
+    doc_ref.set({source['name']: source for source in sources})
