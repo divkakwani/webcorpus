43b30252d09b6de9fb94fe62ddcab1b6fc1959a9
divkakwani <divkakwani@gmail.com>
2021-02-27 20:06:33 +0000

    update scripts

--- scripts/archival.py
+++ scripts/archival.py
@@ -2,19 +2,25 @@
 
 import sys
 import shutil
+import os
+
+from pathlib import Path
 
 op = sys.argv[1]
 dirname = sys.argv[2]
 
 
 if op == 'compress':
+    compressed_path = os.path.join(Path(dirname).parent, Path(dirname).name + '_compressed')
+    os.mkdir(compressed_path)
     for d in os.listdir(dirname):
-        compressed_path = os.path.join(dirname, '_compressed')
-        os.mkdir(compressed_path)
-        shutil.make_archive(os.path.join(compressed_path, d), 'xztar', os.path.join(dirname, d))
-else op == 'decompress':
+        shutil.make_archive(base_name=os.path.join(compressed_path, d),
+                            format='xztar',
+                            root_dir=dirname,
+                            base_dir=d)
+elif op == 'decompress':
+    uncompressed_path = Path(dirname).stem + '_uncompressed'
+    os.mkdir(uncompressed_path)
     for arc in os.listdir(dirname):
-        uncompressed_path = os.path.join(dirname, '_compressed')
-        os.mkdir(uncompressed_path)
         shutil.unpack_archive(os.path.join(dirname, arc), uncompressed_path, 'xztar')
 
--- scripts/arts.py
+++ scripts/arts.py
@@ -1,14 +1,17 @@
 
 import os
 import sys
+from pathlib import Path
 
 from webcorpus.processors.arts import ArtsProcessor
+from webcorpus.processors.sent import SentProcessor
 
 
 
-lang = sys.argv[1]
-dirname = sys.argv[2]
+# lang = sys.argv[1]
+# dirname = '/home/divkakwani/' + lang
+# outdir = '/home/divkakwani/' + lang + '_arts'
 
-
-proc = ArtsProcessor(lang, dirname, os.path.join(dirname, '_arts'))
+# proc = ArtsProcessor(lang, dirname, outdir) 
+proc = SentProcessor('as', '/home/divkakwani/as_old', '/home/divkakwani/as_sent') 
 proc.gen_dataset()
