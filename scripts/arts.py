a8802f791b35477732d4cffe4b931b3cd38f59d4
Divyanshu Kakwani <divkakwani@gmail.com>
2021-03-01 00:30:47 +0530

    update art script

--- scripts/arts.py
+++ scripts/arts.py
@@ -4,14 +4,12 @@ import sys
 from pathlib import Path
 
 from webcorpus.processors.arts import ArtsProcessor
-from webcorpus.processors.sent import SentProcessor
 
 
 
-# lang = sys.argv[1]
-# dirname = '/home/divkakwani/' + lang
-# outdir = '/home/divkakwani/' + lang + '_arts'
+lang = sys.argv[1]
+dirname = '/home/divkakwani/' + lang
+outdir = '/home/divkakwani/' + lang + '_arts'
 
-# proc = ArtsProcessor(lang, dirname, outdir) 
-proc = SentProcessor('as', '/home/divkakwani/as_old', '/home/divkakwani/as_sent') 
-proc.gen_dataset()
+proc = ArtsProcessor(lang, dirname, outdir) 
+proc.run()
